8806ef86d1b455f2362cd65a9ec8e3b7ce5e015f
Ha <630019@bah.com>
2023-08-02 15:19:44 -0500

    Created components folder

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,25 @@
 <script>
-	let firstName = "Ichigo";
-	let lastName = "Kurosaki";
-	let color = "blue";
+  import FeedbackList from "./components/FeedbackList.svelte";
 
-	$: name = firstName + ' ' + lastName;
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+  ];
 </script>
 
 <main>
-	<h1 style="color: {color}">Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <FeedbackList {feedback}/>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
